26677385e51c55106d8ea084777208e50bc4ad9b
stoull <changchunstep@gmail.com>
2020-11-22 23:01:32 +0800

    session

--- main.py
+++ main.py
@@ -139,8 +139,34 @@ def cookies():
 
 	# Storing cookies
 	resp = make_response(f"here how to handle cookies. The cookie which your sent to me: {request.cookies}")
-	resp.set_cookie('username', 'the users name')
+	resp.set_cookie('username', 'hut')
 	return resp
 
+from flask import session
+# Set the secret key to some random bytes. Keep this really secret!
+app.secret_key =  b'_5#y2L"F4Q8z\n\xec]/'
+
+@app.route('/session/')
+def session_s():
+	if 'hut' in session:
+		return 'Logged in as %s' % escape(session['username'])
+	return 'You are not logged in'
+
+@app.route('/log_in/', methods=['GET', 'POST'])
+def log_in():
+	if request.method == 'POST':
+		session['username'] = request.form['username']
+		return redirect(url_for(index))
+	return '''
+			<form method="post">
+            <p><input type=text name=username>
+            <p><input type=submit value=Login>
+        </form>
+    '''
+    
+@app.route('/logout/')
+def logout():
+	session.pop('username', None)
+	return redirect(url_for('index'))    
 
 
